e472cab5b67edc17fef794cb9334679855a58992
seren~dipity <202597942@qq.com>
2023-06-04 15:07:23 +0800

    测试

---
+++ 1-Statistics/cmake-build-debug/CMakeFiles/1_Statistics.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_Statistics.exe"
+  "1_Statistics.exe.manifest"
+  "1_Statistics.pdb"
+  "CMakeFiles/1_Statistics.dir/main.c.obj"
+  "lib1_Statistics.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1_Statistics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
