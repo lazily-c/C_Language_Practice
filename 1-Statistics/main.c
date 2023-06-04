6b9e9d3fd4e988479575cb4523692421de82e1e9
seren~dipity <202597942@qq.com>
2023-06-04 15:55:10 +0800

    统计数据的练习

--- 1-Statistics/main.c
+++ 1-Statistics/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-
+//问题：输出N个数(N小于等于100)，输出数字2出现的次数
 int main() {
-    printf("Hello, World!\n");
+    int element_count;//元素个数
+    int arr[100];
+    scanf("%d",&element_count);
+    int i;
+    for(i=0;i<element_count;i++)
+    {
+        scanf("%d",&arr[i]);//循环读取多个整型元素
+    }
+    //判断整型数组中2出现的次数
+    int count=0;//记录2出现的次数
+    for(i=0;i<element_count;i++)
+    {
+        if(arr[i]==2)
+        {
+            count++;
+        }
+    }
+    printf("%d\n",count);
     return 0;
 }
