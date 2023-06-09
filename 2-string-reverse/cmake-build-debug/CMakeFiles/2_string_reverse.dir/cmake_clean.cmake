4bbf07961cadb7ea8eb3ce8bb51261d80b3390de
seren~dipity <202597942@qq.com>
2023-06-09 14:33:47 +0800

    练习字符翻转后，与原字符串进行比较

---
+++ 2-string-reverse/cmake-build-debug/CMakeFiles/2_string_reverse.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_string_reverse.exe"
+  "2_string_reverse.exe.manifest"
+  "2_string_reverse.pdb"
+  "CMakeFiles/2_string_reverse.dir/main.c.obj"
+  "lib2_string_reverse.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2_string_reverse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
