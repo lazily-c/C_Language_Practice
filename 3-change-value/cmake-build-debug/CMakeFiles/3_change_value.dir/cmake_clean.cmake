1cdd0e1faba1dae0a59ffeb642f6f664a396fabe
seren~dipity <202597942@qq.com>
2023-06-11 16:23:22 +0800

    1、子函数改变main函数的值 2、malloc的使用

---
+++ 3-change-value/cmake-build-debug/CMakeFiles/3_change_value.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_change_value.exe"
+  "3_change_value.exe.manifest"
+  "3_change_value.pdb"
+  "CMakeFiles/3_change_value.dir/main.c.obj"
+  "lib3_change_value.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3_change_value.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
