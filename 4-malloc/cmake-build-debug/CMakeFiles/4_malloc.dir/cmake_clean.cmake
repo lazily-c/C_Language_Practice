7b4e03e4c49436e47d453672816871c78509989f
seren~dipity <202597942@qq.com>
2023-06-14 19:21:48 +0800

    malloc的使用

---
+++ 4-malloc/cmake-build-debug/CMakeFiles/4_malloc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4_malloc.exe"
+  "4_malloc.exe.manifest"
+  "4_malloc.pdb"
+  "CMakeFiles/4_malloc.dir/main.c.obj"
+  "lib4_malloc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/4_malloc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 4-malloc/cmake-build-debug/CMakeFiles/4_malloc.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 4_malloc.
+# This may be replaced when dependencies are built.
